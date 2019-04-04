ed9ff0f78dabe98ccd7551e317d603bff42cdd39
vuong912 <ltvshy@gmail.com>
2019-04-04 23:22:38 +0700

    update

--- demo_microservice/customers/main.go
+++ demo_microservice/customers/main.go
@@ -10,11 +10,20 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type servicesHost struct {
+	BooksHost string
+}
+
+var ServicesHost = servicesHost{
+	BooksHost: "books.local",
+}
+
 func GetIdHandler(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	data := "User " + params["id"] + "th have "
 
-	resp, err := Request("GET", "http://192.168.99.100/books/"+params["id"], nil, "books.local")
+	resp, err := Request("GET", "http://192.168.99.100/books/"+params["id"], nil, ServicesHost.BooksHost)
+	//resp, err := Request("GET", "http://127.0.0.1:80/books", nil, ServicesHost.BooksHost)
 	if err == nil {
 		w.Write([]byte(data + string(resp) + "~~~"))
 	} else {
@@ -43,5 +52,5 @@ func Request(method string, path string, body io.Reader, host string) ([]byte, e
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/customers/{id}", GetIdHandler).Methods("GET")
-	log.Fatal(http.ListenAndServe("0.0.0.0:8080", router))
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
